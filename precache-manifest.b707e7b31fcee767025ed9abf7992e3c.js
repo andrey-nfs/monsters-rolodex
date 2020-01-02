5c82e63784009a52d0389b17526850899982f0b3
Andrey Ignatyev <ignatyev@andrey.com.au>
2020-01-02 20:03:32 +1100

    Updates

---
+++ precache-manifest.b707e7b31fcee767025ed9abf7992e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364c2ce45967a6090f64d0469ba99d91",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f42341ffb06a357ac8e9",
+    "url": "/monsters-rolodex/static/css/main.b25594b9.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f42341ffb06a357ac8e9",
+    "url": "/monsters-rolodex/static/js/main.8fce206d.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
